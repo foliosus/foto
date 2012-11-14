c54725fba19c6f484bb8ee294affa2329f70aac2
Peter Tran <Peter Tran ptran@optimiscorp.com>
2012-11-14 13:50:14 -0800

    Adding more specs

--- spec/foto/config_spec.rb
+++ spec/foto/config_spec.rb
@@ -21,6 +21,24 @@ describe Foto::Config do
     end
   end
 
+  describe '.reset!' do
+    it 'resets the configuration to their default values' do
+      config.api_key = 'somethingelse'
+      config.api_key.should eql('somethingelse')
+      config.patients_uri = 'http://other.com'
+      config.patients_uri.should eql('http://other.com')
+      config.reset!
+      config.api_key.should eql(defaults::API_KEY)
+      config.patients_uri.should eql(defaults::PATIENTS_URI)
+    end
+  end
+
+  describe '.options' do
+    it 'returns a hash of current options' do
+      config.options.keys.should =~ Foto::Config::VALID_OPTIONS
+    end
+  end
+
   describe '.configure' do
     Foto::Config::VALID_OPTIONS.each do |option|
       it "#{option} should be configurable" do
