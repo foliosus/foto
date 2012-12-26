6c5aa946f91633e073d34698905927b20faae6e9
Peter Tran <Peter Tran ptran@optimiscorp.com>
2012-12-26 14:34:03 -0800

    Better exception handling

--- lib/foto/requests/response.rb
+++ lib/foto/requests/response.rb
@@ -3,14 +3,18 @@ module Foto
     class Response
       attr_reader :body, :code
 
+      class InvalidResponse < StandardError; end
+
       def initialize(http_response)
         @body = http_response.body
         @code = http_response.code
       end
 
       def message
-        doc = Nokogiri::HTML(@body)
+        doc = Nokogiri::HTML(body)
         doc.xpath('/html/body/p').first.content
+      rescue Exception => e
+        raise InvalidResponse, "The response from FOTO could not be parsed.\nHTTP #{code} : #{body}"
       end
 
       def successful?
